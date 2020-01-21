0a12e18b417997edbf358d2757bfe6a64a998d10
Zendelle Badiang <Zendelle@udel.edu>
2020-01-20 19:36:57 -0500

    Addition of UILabel for EmojiPerMinute/ Addition of WPM logic/ Changed the EMoji Label to take in String comprised of Array objects instead of Array itself

--- Zappos_EmojiPerMinute/Controller/ViewController.swift
+++ Zappos_EmojiPerMinute/Controller/ViewController.swift
@@ -20,27 +20,31 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var UserInput:UITextField!
     @IBOutlet weak var Emoji_Display: UILabel!
+    @IBOutlet weak var WordPerMinuteLabel: UILabel!
     
-    
+    //Variables
     var Emoji_String = ""
     var charEmojis = Set<String>()
+    var score: Int = 0
+    var totalKeyStrokes: Int = 0
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        //UITextField
+        UserInput.sizeToFit()
         
-        UserInput.isEnabled = false
+        WordPerMinuteLabel.sizeToFit()
         
         
-        // Do any additional setup after loading the view.
         
         /* Asychronous!! Runs in the background so makesure to time your functions with
          ex. Enable text box to appear all all functions regarding logic once the api request responds succesfully*/
         getNewEmojis(url: ResourceURL, count: count)
         
-
-
+        
         
     }
 
@@ -63,7 +67,7 @@ class ViewController: UIViewController {
                 // Cast response.result.value! as a JSON
                 let EmojiJSON : JSON = JSON(response.result.value!)
                 
-                self.UserInput.isEnabled = true
+                self.UserInput.becomeFirstResponder()
                 tempJSON = EmojiJSON
                 
                 //Updates EMOJI array with new set of emojis.
@@ -87,28 +91,63 @@ class ViewController: UIViewController {
         */
         for i in json["emojis"].arrayValue{
             tempArr.append(i.stringValue)
+            Emoji_String += i.stringValue
         }
         
         emojiDataModel.setEmojis(arr: tempArr)
         
-        for i in json["emojis"].arrayValue{
-            charEmojis.insert(i.stringValue)
-        }
-        
         print(emojiDataModel.emojis)
-        Emoji_Display.text = "\(emojiDataModel.emojis)"
+        Emoji_Display.text = Emoji_String
         Emoji_Display.sizeToFit()
 
     }
     
     
     //MARK: - IBAction User Input
+    /**
+            Everytime UITextField is changed, checks if Emoji_String has substring Sender.Text
+        Function:
+     Counts totalKeyStrokes
+     Count total Correct Matches
+     Starts timer if not already started
+     @return void
+     */
     @IBAction func InputEdited(_ sender: UITextField) {
         
+        /*
+         sudo code: checks if a timer exists, instances one if not
+         */
         
+        if Emoji_String.hasPrefix(sender.text!){
+            score += 1
+        }
+        else{
+            score -= 1
+        }
+        totalKeyStrokes += 1
+        print(score)
+        print(totalKeyStrokes)
+        emojiPerMinute(totalKeyStrokes: totalKeyStrokes, correctKeys: totalKeyStrokes-1)
         
     }
     
+    
+    //MARK: - Emoji Per Minute Calculator
+    /**
+     Number_of_keystroke / time_in_minute * percentages_of_accurate_EMOJI
+     */
+    func emojiPerMinute(totalKeyStrokes: Int, correctKeys: Int){
+        
+        let epm = Double(totalKeyStrokes / 1 * correctKeys)
+        let remainder = epm.truncatingRemainder(dividingBy: 1)
+        if remainder >= 0.5{
+            WordPerMinuteLabel.text =  "\(epm + Double(1 - remainder))) EMP"
+        }
+        else{
+            WordPerMinuteLabel.text =  "\(epm) EPM"
+        }
+        
+    }
     
     
 }
